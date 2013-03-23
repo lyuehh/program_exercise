bd8567502d0b07939e0a669309c897b6c71334e4
lyuehh <lyuehh@gmail.com>
2013-03-23 22:23:11 +0800

    ruby check html content & tips

---
+++ ruby/yyets/sibadakesi.rb
@@ -0,0 +1,20 @@
+require 'open-uri'
+require 'nokogiri'
+
+def check
+  doc = Nokogiri::HTML(open('http://www.yyets.com/resource/11176'))
+  doc.css('.f14').each do |l|
+    if l.to_s.include?('晚上')
+      puts 'no'
+      sleep 60 * 10
+    else
+      `terminal-notifier -message "可以看了..."`
+      puts "haha"
+      exit
+    end
+  end
+end
+
+while true
+  check
+end
