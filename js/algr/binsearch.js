63ab899954df27ed70067ed1ccbeeb8de915a173
lyuehh <lyuehh@gmail.com>
2013-03-25 21:43:28 +0800

    add binsearch version2, rename test file

--- js/algr/binsearch.js
+++ js/algr/binsearch.js
@@ -2,11 +2,7 @@ function binsearch(arr,x) {
   var high = arr.length - 1;
   var low = 0;
   while(low <= high) {
-    var mid1 = Math.floor((high+low)/2);
     var mid = parseInt((high+low)/2, 10);
-    console.log('');
-    console.log('1: ' + mid1);
-    console.log('2: ' + mid);
     if(x > arr[mid]) {
       low = mid + 1;
     } else if(x < arr[mid]) {
@@ -18,4 +14,24 @@ function binsearch(arr,x) {
   return -1;
 }
 
+function binsearch2(arr, x) {
+  var high = arr.length - 1;
+  var low = 0;
+  var mid = parseInt((high+low)/2, 10);
+  while(low <= high && x !== arr[mid]) {
+    if(x < arr[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    mid = parseInt((high+low)/2, 10);
+  }
+  if(x === arr[mid]) {
+    return mid;
+  } else {
+    return -1;
+  }
+}
+
 exports.binsearch = binsearch;
+exports.binsearch2 = binsearch2;
