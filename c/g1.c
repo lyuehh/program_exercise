6dab78eadaea58a4b2d629e68bf08b4c8b1ce5e8
lyuehh <lyuehh@gmail.com>
2013-04-01 16:25:52 +0800

    add google str algor test 1

---
+++ c/g1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int min_next_str(char s[]);
+int min_next_str2(char s[]);
+
+int main(int argc, const char *argv[])
+{
+  char s1[] = "abc";
+  char s2[] = "aby";
+  char s3[] = "abz";
+  min_next_str2(s1);
+  min_next_str2(s2);
+  min_next_str2(s3);
+  printf("%s\n", s1);
+  printf("%s\n", s2);
+  printf("%s\n", s3);
+  return 0;
+}
+
+int min_next_str(char s[]) {
+  int l = strlen(s) - 1;
+  while(l >= 0) {
+    s[l]++;
+    if(s[l] <= 'z') {
+      break;
+    }
+    l--;
+  }
+  if(l<0) {
+    return 1;
+  } else {
+    s[++l] = '\0';
+    return 1;
+  }
+}
+
+int min_next_str2(char s[]) {
+  int l = strlen(s) - 1;
+  while(l >= 0) {
+    if(s[l] == 'z') {
+      l--;
+    } else {
+      s[l]++;
+      break;
+    }
+  }
+  if(l<0) {
+    return 1;
+  } else {
+    s[++l] = '\0';
+    return 1;
+  }
+}
