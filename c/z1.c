412b62c6ef9beeefa4ddecf8d4494aa491f28052
lyuehh <lyuehh@gmail.com>
2013-04-04 15:48:13 +0800

    remove todo, add fire.rb

---
+++ c/z1.c
@@ -0,0 +1,11 @@
+/*
+编码实现字符串右移n位，如“diopHeg”右移2位为“egdiopH”
+ * */
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+  
+  return 0;
+}
--- js/todo/app/scripts/controllers/tips.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('todoApp')
-  .controller('TipsCtrl', function ($scope) {
-    //$('.btn1').tips('aaabbb');
-    $('.btn1').tips('<script>alert("123")</script>');
-    //$('.link1').tips('aaabbb');
-  });
