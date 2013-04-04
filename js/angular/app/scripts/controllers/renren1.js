22f63aeedbe5175134484702f601909bc2a57bea
lyuehh <lyuehh@gmail.com>
2013-04-04 18:12:00 +0800

    add renren js question 1

--- js/angular/app/scripts/controllers/renren1.js
+++ js/angular/app/scripts/controllers/renren1.js
@@ -1,6 +1,51 @@
 'use strict';
 
-angular.module('todoApp')
-  .controller('RenRen1', function ($scope) {
-    console.log('renren1');
+angular.module('todoApp').controller('RenRen1', function () {
+  var ul = $('.ul')[0];
+  var li0 = $(ul).children()[0];
+  var li1 = $(ul).children()[1];
+
+  var $left = $('.left');
+  var $right = $('.right');
+
+  $left.click(function() {
+    sleft();
   });
+  $right.click(function() {
+    sright();
+  });
+
+  function sleft() {
+    ul.scrollLeft += -70;
+    if(ul.scrollLeft <=0 ) {
+      $left.addClass('disable');
+    } else {
+      $right.removeClass('disable');
+    }
+  }
+
+  function sright() {
+    ul.scrollLeft += 70;
+    if(ul.scrollLeft >= (ul.scrollWidth - ul.clientWidth)) {
+      $right.addClass('disable');
+    } else {
+      $left.removeClass('disable');
+    }
+  }
+
+  // $(ul).scroll(function(e) {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  // });
+
+  ul.addEventListener(('onmousewheel' in ul) ? 'mousewheel' : 'DOMMouseScroll', function(e) {
+    if((e.detail || -e.wheelDelta) > 0) {
+      sright();
+    } else {
+      sleft();
+    }
+  }, true);
+  window.ul = ul;
+  window.li0 = li0;
+  window.li1 = li1;
+});
