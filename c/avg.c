55fcc4306a1cf528c322f6b2a7d29530238b0c1b
lyuehh <lyuehh@gmail.com>
2013-10-30 12:40:06 +0800

    update avg.c

---
+++ c/avg.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int x, n = 0, min, max, s = 0;
+    scanf("%d", &x);
+    min = max = x;
+    while (scanf("%d", &x) == 1) {
+        s += x;
+        if (x < min) {
+            min = x;
+        }
+        if (x > max) {
+            max = x;
+        }
+        n++;
+    }
+    printf("%d %d %.3f\n", min, max, (double)s/n);
+    return 0;
+}
