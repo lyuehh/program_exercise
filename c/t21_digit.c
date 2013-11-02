75e390331bf01cd68ba4d59a202f5c9dc07c71b5
lyuehh <lyuehh@gmail.com>
2013-11-02 21:00:01 +0800

    add t2-1

--- c/t21_digit.c
+++ c/t21_digit.c
@@ -3,7 +3,12 @@
 int main(int argc, const char *argv[])
 {
     int n;
+    int d = 1;
     scanf("%d", &n);
-    printf("%d\n", n);
+    while (n > 10) {
+        n = n / 10;
+        d++;
+    }
+    printf("%d\n", d);
     return 0;
 }
