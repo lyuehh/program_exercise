65ce0d7f1e03881578165ba1e8bff412f86fdedd
lyuehh <lyuehh@gmail.com>
2013-11-07 21:33:34 +0800

    add t3-1 2

--- c/art/t31_stat_2.c
+++ c/art/t31_stat_2.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 1000
+#define MAX 10000 + 10
 int a[MAX];
 
 
 int main(int argc, const char *argv[])
 {
-    memset(a, -1, sizeof(a));
-    int n;
+    memset(a, 0, sizeof(a));
+    float f;
     int i = 0;
+    int max = 0;
 
-    while(scanf("%d", &n) == 1) {
-        a[i] = n;
+    while(scanf("%f", &f) == 1) {
+        int n = f * 100;
+        a[n]++;
+    }
+    for (i=0; i<MAX; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    for (i=0; i<MAX; i++) {
+        if (a[i] == max) {
+            printf("%.2f ", i / 100.0);
+        }
     }
     return 0;
 }
