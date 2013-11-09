c5ccbcda07a8528116bf58c16396ec373cd26330
lyuehh <lyuehh@gmail.com>
2013-11-09 20:35:52 +0800

    add t 3-5 part 1

---
+++ c/art/t35_rotate.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+char a[MAX][MAX];
+
+int main(int argc, const char *argv[])
+{
+    char c;
+    int row;
+    while (scanf("%c", &c) != EOF) {
+    }
+    printf("row: %d\n", row);
+    return 0;
+}
