30667242c60041a27acd6c50db20b8a401e52cbe
lyuehh <lyuehh@gmail.com>
2013-11-09 22:32:15 +0800

    update t 3-5

--- c/art/t35_rotate.c
+++ c/art/t35_rotate.c
@@ -2,14 +2,64 @@
 #include <string.h>
 
 #define MAX 1000
-char a[MAX][MAX];
+char a[5][5] = {
+    {1,2,3,4,5},
+    {6,7,8,9,10},
+    {11,12,13,14,15},
+    {16,17,18,19,20},
+    {21,22,23,24,25}
+};
+char b[5][5];
 
 int main(int argc, const char *argv[])
 {
-    char c;
-    int row;
-    while (scanf("%c", &c) != EOF) {
+    int i, j;
+    int n = 5;
+    for (i=0; i<n; i++) {
+        for (j=0; j<n; j++) {
+            printf("%2d ", a[i][j]);
+            if (j == 4) {
+                printf("\n");
+            }
+        }
     }
-    printf("row: %d\n", row);
+    printf("-----\n");
+    /*
+    // 保存第1排的
+    for (i=0; i<n; i++) {
+        b[i] = a[0][i];
+    }
+    // 最右竖着一排到第1排
+    for (i=0; i<n; i++) {
+        a[0][i] = a[i][n-1];
+    }
+    // 最下一排到最右一排
+    for (i=0; i<n; i++) {
+        a[i][n-1] = a[n-1][n-i-1];
+    }
+    // 最左一排到最下
+    for (i=0; i<n; i++) {
+        a[n-1][n-i-1] = a[n-i-1][0];
+    }
+    // 把保存的最上放到最左
+    for (i=0; i<n; i++) {
+        a[i][0] = b[n-i-1];
+    }
+    */
+
+    for (i=0; i<n; i++) {
+        for (j=0; j<n; j++) {
+            b[i][j] = a[j][n-i-1];
+        }
+    }
+    printf("-----\n");
+    for (i=0; i<n; i++) {
+        for (j=0; j<n; j++) {
+            printf("%2d ", b[i][j]);
+            if (j == 4) {
+                printf("\n");
+            }
+        }
+    }
     return 0;
 }
