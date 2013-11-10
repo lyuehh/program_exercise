a5c54a3cd4f8e23001e4023c95d0fb5c19fe6d7a
lyuehh <lyuehh@gmail.com>
2013-11-10 20:48:10 +0800

    add 4-1

---
+++ c/art/41_zuhe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int f(int n)
+{
+    int i, m = 1;
+    for (i=1; i<=n; i++) {
+        m *= i;
+    }
+    return m;
+}
+
+int main(int argc, const char *argv[])
+{
+    int m, n;
+    scanf("%d%d", &m, &n);
+    printf("%d\n", f(n) / (f(m)*f(n-m)));
+    return 0;
+}
