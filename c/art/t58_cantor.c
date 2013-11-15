306b41019f6683818b3a21f49bb38b2dc6d42bcf
lyuehh <lyuehh@gmail.com>
2013-11-15 09:49:26 +0800

    add 5-8

---
+++ c/art/t58_cantor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int n;
+    while (scanf("%d", &n) == 1) {
+        int k = 1, s = 0;
+        for (;;) {
+            s += k;
+            if (s >= n) {
+                printf("%d/%d\n", s-n+1, k-s+n);
+                break;
+            }
+            k++;
+        }
+    }
+    return 0;
+}
