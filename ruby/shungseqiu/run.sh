b21c53dcd73fa40e958fac18b3b90bca4268a7ac
lyuehh <lyuehh@gmail.com>
2014-01-19 17:45:44 +0800

    update run.sh

--- ruby/shungseqiu/run.sh
+++ ruby/shungseqiu/run.sh
@@ -4,5 +4,5 @@ for i in `seq 2003 2014`
 do
     u="http://www.3dcp.cn/zs/gonggao.php?type=ssq&year=$i"
     echo $u
-    curl -s $u | iconv -f gbk -t utf8  | hquery -e 'table' -c '.eq(13).parent().html()' > $i.html
+    curl -s $u | iconv -f gbk -t utf8  | hquery -p -r '$("table").eq(13).parent().html()'
 done
