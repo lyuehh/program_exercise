14c8be726173d77bfd1302592276d0cb5f06bfa0
lyuehh <lyuehh@gmail.com>
2014-02-12 09:34:44 +0800

    update repeat.js

--- js/repeat.js
+++ js/repeat.js
@@ -1,11 +1,13 @@
 function repeat(fn, time, wait) {
     return function(s) {
+        var args = Array.prototype.slice.call(arguments);
         for(var i=0; i<time; i++) {
-            (function(i){
+            // (function(i){ // 这个立即调用不是必须的
                 setTimeout(function() {
-                    fn(s);
-                }, i * wait);
-            })(i);
+                    fn.apply(null, args);
+                    // console.log(i); // 如果不立即调用, 那么这里的i 一直是10
+                }, i * wait); // 这里的i会循环
+            // })(i);
         }
     };
 }
