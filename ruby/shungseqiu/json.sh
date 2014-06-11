bb58407b5541ab3536f4c170aed652ff3d6ccbf2
lyuehh <lyuehh@gmail.com>
2014-06-11 14:21:36 +0800

    update shuangseqiu

--- ruby/shungseqiu/json.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in `seq 2003 2014`
-do
-    echo $i
-    cat ${i}.html | hquery -p -f a.js | sed '$d' > json/${i}.json
-done
--- ruby/shungseqiu/run.sh
+++ ruby/shungseqiu/run.sh
@@ -3,6 +3,8 @@
 for i in `seq 2003 2014`
 do
     u="http://www.3dcp.cn/zs/gonggao.php?type=ssq&year=$i"
-    echo $u
-    curl -s $u | iconv -f gbk -t utf8  | hquery -p -r '$("table").eq(13).parent().html()' > $i.html
+    # echo $u
+    # curl -s $u | iconv -f gbk -t utf8  | hquery -p -r '$("table").eq(13).parent().html()' > $i.html
+
+    curl -s  http://www.3dcp.cn/zs/gonggao.php\?type\=ssq\&year\=$i |  iconv -f gbk -t utf8  | hquery -p -r '$("table").eq(13).parent().html()' | hquery -p -f b.js
 done
